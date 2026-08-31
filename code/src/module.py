6252bd8525f2bd40405a4ebb737a09a6df2055a9
tierneydaw1 <tierneydaw@ucla.edu>
2026-08-31 22:31:01 +0000

    "Add code to decode stim"

--- code/src/module.py
+++ code/src/module.py
@@ -45,3 +45,30 @@ def neuron_psth(units_table, neuron_row_index, stim_time_array, psth_bin_edge_ar
     count_array = get_binned_triggered_spike_counts_fast(
         spike_time_array, stim_time_array, psth_bin_edge_array)
     return count_array.mean(axis=0) / 0.05
+
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion(y_true, y_pred, label_array, title):
+    count_array = confusion_matrix(y_true, y_pred, labels=label_array)          # raw counts
+    fraction_array = count_array / count_array.sum(axis=1, keepdims=True)       # row-normalised (each true class sums to 1)
+    fig, ax = plt.subplots(figsize=(5.2, 4.6))
+    mesh = ax.pcolormesh(fraction_array, cmap='viridis', alpha=0.7,
+                         edgecolor='white', linewidth=2)                        # colored cells, white edges
+    for r in range(len(label_array)):                                          # write count + percent in each cell
+        for c in range(len(label_array)):
+            ax.text(c + 0.5, r + 0.5, '%d\n%.0f%%'
+                    % (count_array[r, c], 100 * fraction_array[r, c]),
+                    ha='center', va='center', color='black')
+    ax.set_xticks(np.arange(len(label_array)) + 0.5)
+    ax.set_yticks(np.arange(len(label_array)) + 0.5)
+    ax.set_xticklabels(label_array)
+    ax.set_yticklabels(label_array)
+    ax.invert_yaxis()                                                          # put first class at top
+    ax.set_xlabel('stimulus predicted by the decoder')
+    ax.set_ylabel('stimulus actually shown')
+    ax.set_title(title)
+    cbar = fig.colorbar(mesh, ax=ax)
+    cbar.set_label('fraction of true-class\n trials assigned to this class')
+    fig.tight_layout()
+    plt.show()
